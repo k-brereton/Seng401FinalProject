4a4905e7a3888e365660ba65eec45ca3269752bc
Kevin Brereton <kevin.brereton@ucalgary.ca>
2019-03-30 14:40:58 -0600

    added minor documentation

--- app/Http/Controllers/TwitchUserController.php
+++ app/Http/Controllers/TwitchUserController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Helpers\TwitchDriver;
+//tuser means twitch user. 
 
+//this is the class which controls how users find each twitch user (tuser for short)
 class TwitchUserController extends Controller
 {
     public function findByGame($game_id){
         $streams=TwitchDriver::sendTwitchRequest('/streams',['game_id' => $game_id]);
         return view('tuser/tuserListView',['streams'=> $streams->data]);
     }
+    //views a single twitch user. 
     public function viewTuser($tuser_id){
         $tuser=TwitchDriver::sendTwitchRequest('/users',['id' => $tuser_id]);
         $user=\Auth::user();
-        
+        //if nobody is logged in, show not logged in page.
         if(is_null($user)){
             return view('tuser/singleTuserNotLogedIn', ['tuser' => $tuser->data[0]]);
         }
