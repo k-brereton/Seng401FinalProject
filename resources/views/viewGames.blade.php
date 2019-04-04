1e699b16bddd4e5828de932ddcd266ad1fad3708
ZackSSheppard <zack.sheppard@gmail.com>
2019-04-04 14:03:55 -0600

    game images, more games

--- resources/views/viewGames.blade.php
+++ resources/views/viewGames.blade.php
@@ -3,6 +3,13 @@
 @section('content')
 @foreach($games as $game)
 <a href='tusers/byGame/{{$game->id}}'>{{$game->name}}</a>
+<?php
+$w=150;
+$h=200;
+$string=str_replace('{width}', $w, $game->box_art_url);
+$string=str_replace('{height}', $h, $string);
+?>
+<img src='{{$string}}' alt='{{$game->name}}'>
 
 @endforeach
 
