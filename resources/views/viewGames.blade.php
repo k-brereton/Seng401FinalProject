8da161984bf03538b4623b7deffda2a90e057e59
ZackSSheppard <zack.sheppard@gmail.com>
2019-04-07 15:44:21 -0600

    cleaned up game page

--- resources/views/viewGames.blade.php
+++ resources/views/viewGames.blade.php
@@ -2,14 +2,26 @@
 
 @section('content')
 @foreach($games as $game)
-<a href='tusers/byGame/{{$game->id}}'>{{$game->name}}</a>
-<?php
-$w=150;
-$h=200;
-$string=str_replace('{width}', $w, $game->box_art_url);
-$string=str_replace('{height}', $h, $string);
-?>
-<img src='{{$string}}' alt='{{$game->name}}'>
+<!-- Shows 8 games per row-->
+<div style='float:left; width:12.5%;'>
+  <!-- Game titles -->
+  <p style="text-align:center; width:100%; height:30px">
+    <a href='tusers/byGame/{{$game->id}}'>{{$game->name}}</a>
+  </p>
+
+  <?php // Replace picture height/width parameters
+    $w=170;
+    $h=225;
+    $string=str_replace('{width}', $w, $game->box_art_url);
+    $string=str_replace('{height}', $h, $string);
+  ?>
+
+  <!-- Game pictures -->
+  <a href='tusers/byGame/{{$game->id}}'>
+    <img style="display:block; margin:auto; margin-bottom:45px;"
+         src='{{$string}}' alt='{{$game->name}}'>
+  </a>
+</div>
 
 @endforeach
 
