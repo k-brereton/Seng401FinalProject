d0c64281731c6fbb6d371f260e4293dc0e50671f
mglyngberg <mglyngberg2@gmail.com>
2019-04-10 19:58:42 -0600

    Add user stream to subscribe page

--- resources/views/tuser/singleTuser.blade.php
+++ resources/views/tuser/singleTuser.blade.php
@@ -3,19 +3,29 @@
 
 @section('content')
 
+<div style="overflow:auto; width:800px; margin: auto;">
+	<img src="{{$tuser->profile_image_url}}" alt="Channel Image" width="80" style="float:left">
+	<h1 style="position: relative; top:15px; width: 300px; float:left;">{{$tuser->display_name}}</h1>
+	@if(!$user->isSubscribedToTuser($tuser->id))
+	<form action='/tusers/{{$tuser->id}}/subscribe' method="post">
+	    {{csrf_field()}}
+	<input type="submit" value="Subscribe" style="float:right; height:80px; font-size : 25px;">
+	</form>
+	@else
+	<form action='/tusers/{{$tuser->id}}/unsubscribe' method="post">
+	    {{csrf_field()}}
+	<input type="submit" value="Unsubscribe" style="float:right; height:80px; font-size : 25px;">
+	</form>
+	@endif
 
-{{$tuser->login}}
-@if(!$user->isSubscribedToTuser($tuser->id))
-<form action='/tusers/{{$tuser->id}}/subscribe' method="post">
-    {{csrf_field()}}
-<input type="submit" value="subscribe" >
-</form>
-@else
-<form action='/tusers/{{$tuser->id}}/unsubscribe' method="post">
-    {{csrf_field()}}
-<input type="submit" value="unsubscribe">
-</form>
-
-@endif
-
+	<!-- <img src="{{$tuser->offline_image_url}}" alt="Channel Image" width="800"> -->
+	<iframe
+    	src="https://player.twitch.tv/?channel={{$tuser->display_name}}&autoplay=false"
+	    height="450"
+	    width="800"
+	    frameborder="<frameborder>"
+	    scrolling="<scrolling>"
+	    allowfullscreen="<allowfullscreen>">
+</iframe>
+</div>
 @endsection
