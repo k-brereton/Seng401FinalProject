8082c44434b41a6cb5ae60b8dea5e4c661f3d9c5
mglyngberg <mglyngberg2@gmail.com>
2019-04-10 21:46:29 -0600

    Stream list looks good now

--- resources/views/tuser/tuserListView.blade.php
+++ resources/views/tuser/tuserListView.blade.php
@@ -3,7 +3,26 @@
 <!--despite the name of this file, the $channel variable is actually stream, not user. -->
 
 @section('content')
+
+
 @foreach($streams as $stream)
-  <a href='/tusers/{{$stream->user_id}}'><h1>{{$stream->user_name}}</h1> </a>
+<div style="display:inline-grid">
+	<?php 
+
+		$h = 125;
+		$w = 240;
+		$string=str_replace('{width}', $w, $stream->thumbnail_url);
+   		$string=str_replace('{height}', $h, $string);
+	?>
+		<div style="float: left">
+
+			<a href='/tusers/{{$stream->user_id}}'><h2 align="center" style="width: 240px;">{{$stream->user_name}}</h2></a>
+  			<img src="{{$string}}" alt="Channel Image">
+  			<p style="font-size:18px;"> Live: {{$stream->viewer_count}} viewers</p>
+
+		</div>
+  		
+</div>
+
 @endforeach
 @endsection
